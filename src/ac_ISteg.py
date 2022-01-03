6fc5d670751fd3e212db29576da5d9ea93cf1a50
chinmaypant21 <pant.chinu.2@gmail.com>
2022-01-03 23:40:10 +0530

    Critical bug fixes in image steg

--- src/ac_ISteg.py
+++ src/ac_ISteg.py
@@ -1,4 +1,4 @@
-from src.utilSteg import is_encodable,pair,add_header,Image
+from src.utilSteg import is_encodable,pair,Image
 from src.util_Isteg import *
 
 #2pixels for flag -> In first two pixel all rgb component will be even
@@ -6,36 +6,44 @@ from src.util_Isteg import *
 #next 5 pixel for num of pixels in secret image -> 2,2,2  2,2,2  2,2,2  2,2,2  1,1,1
 HEADER_SIZE = 13 #pixels
 
+def encode_message(original_image_pixels,secret_image_pixels,secret_image_size):
+    num_payload_pixels  = len(secret_image_pixels)
+    original_pixel_data,offset = add_header(original_image_pixels,secret_image_size,num_payload_pixels)
+
+    for pixel in secret_image_pixels:
+        first2_msb,next2_msb = pixel_MSB(pixel)
+        original_image_pixels[offset] = encode_data(original_image_pixels[offset],first2_msb)
+        offset += 1
+        original_image_pixels[offset] = encode_data(original_image_pixels[offset],next2_msb)
+        offset += 1
+    
+    return original_image_pixels
+    
+    
 def HideData(orig_img_path='media/sierra.jpg',second_img_path='media/mac.jpg'):
     try:
-        original_image = Image.open(orig_img_path)
+        original_image_pixels = Image.open(orig_img_path)
     except:
         print(f"Unable to open image {orig_img_path}")
         exit(0)
     else:
-        original_pixel_data = list(original_image.getdata())
+        original_pixel_data = list(original_image_pixels.getdata())
         
-    
     try:
         secret_image = Image.open(second_img_path)
-        pass
     except:
         print(f"Unable to open image {second_img_path}")
         exit(0)
     else:
         secret_pixel_data = list(secret_image.getdata())
-        num_payload_pixels = len(secret_pixel_data)
-        
-    # msg_bytes = msg_to_binary(message)
-    
+            
     if is_encodable(original_pixel_data,secret_pixel_data,HEADER_SIZE):
-        # original_pixel_data = add_header(original_pixel_data,secret_pixel_data.size,num_payload_pixels)
-        # encoded_msg_bytes = encode_message(original_pixel_data, msg_bytes)
+        encoded_msg_bytes = encode_message(original_pixel_data,secret_pixel_data,secret_image.size)
         
-        # new_img = Image.new(original_image.mode,original_image.size)
-        # new_img.putdata(tuple(encoded_msg_bytes))
-        # new_img.save('media/test.png')
-        # print("[+] Successfully encoded in image")
+        new_img = Image.new(original_image_pixels.mode,original_image_pixels.size)
+        new_img.putdata(tuple(encoded_msg_bytes))
+        new_img.save('test.png')
+        print("[+] Successfully encoded in image")
         pass
 
     else:
--- src/util_Isteg.py
+++ src/util_Isteg.py
@@ -1,7 +1,8 @@
 #Both height and width will use 3 pixels 222222111
 def encode_to_pixel(pixel,bin_pixel_data):
+    print(len(pixel))
     r,g,b = pixel
-    shift = len(bin_pixel_data)/3
+    shift = len(bin_pixel_data)//3
     
     r = r>>shift
     r = r<<shift    
@@ -23,23 +24,23 @@ def encode_to_pixel(pixel,bin_pixel_data):
     return (r,g,b)
 
 def add_dimensions(original_img_pixels,offset,bin_width,bin_height):
-    original_img_pixels[offset]   = encode_to_pixel(original_img_pixels,bin_width[0:6])
-    original_img_pixels[offset+1] = encode_to_pixel(original_img_pixels,bin_width[6:12])
-    original_img_pixels[offset+2] = encode_to_pixel(original_img_pixels,bin_width[12:15])
+    original_img_pixels[offset]   = encode_to_pixel(original_img_pixels[offset],bin_width[0:6])
+    original_img_pixels[offset+1] = encode_to_pixel(original_img_pixels[offset+1],bin_width[6:12])
+    original_img_pixels[offset+2] = encode_to_pixel(original_img_pixels[offset+2],bin_width[12:15])
     
-    original_img_pixels[offset+3] = encode_to_pixel(original_img_pixels,bin_height[0:6])
-    original_img_pixels[offset+4] = encode_to_pixel(original_img_pixels,bin_height[6:12])
-    original_img_pixels[offset+5] = encode_to_pixel(original_img_pixels,bin_height[12:15])
+    original_img_pixels[offset+3] = encode_to_pixel(original_img_pixels[offset+3],bin_height[0:6])
+    original_img_pixels[offset+4] = encode_to_pixel(original_img_pixels[offset+4],bin_height[6:12])
+    original_img_pixels[offset+5] = encode_to_pixel(original_img_pixels[offset+5],bin_height[12:15])
     
     offset += 6
     return original_img_pixels,offset
 
 def add_data_size(original_img_pixels,offset,bin_payload_size):
-    original_img_pixels[offset]     = encode_to_pixel(original_img_pixels,bin_payload_size[0:6])
-    original_img_pixels[offset+1]   = encode_to_pixel(original_img_pixels,bin_payload_size[6:12])
-    original_img_pixels[offset+2]   = encode_to_pixel(original_img_pixels,bin_payload_size[12:18])
-    original_img_pixels[offset+3]   = encode_to_pixel(original_img_pixels,bin_payload_size[18:24])
-    original_img_pixels[offset+4]   = encode_to_pixel(original_img_pixels,bin_payload_size[24:27])
+    original_img_pixels[offset]     = encode_to_pixel(original_img_pixels[offset],bin_payload_size[0:6])
+    original_img_pixels[offset+1]   = encode_to_pixel(original_img_pixels[offset+1],bin_payload_size[6:12])
+    original_img_pixels[offset+2]   = encode_to_pixel(original_img_pixels[offset+2],bin_payload_size[12:18])
+    original_img_pixels[offset+3]   = encode_to_pixel(original_img_pixels[offset+3],bin_payload_size[18:24])
+    original_img_pixels[offset+4]   = encode_to_pixel(original_img_pixels[offset+4],bin_payload_size[24:27])
     
     offset += 5
     return original_img_pixels,offset
@@ -57,7 +58,7 @@ def add_header(original_img_pixels,payload_dimensions,
                 pixel_data.append(component)
             else:
                 pixel_data.append(component+1)
-        original_img_pixels[i] = pixel_data
+        original_img_pixels[i] = tuple(pixel_data)
         
     pixel_offset = 2
     payload_width,payload_height = payload_dimensions
@@ -68,5 +69,41 @@ def add_header(original_img_pixels,payload_dimensions,
     
     original_img_pixels,pixel_offset = add_dimensions(original_img_pixels,pixel_offset,bin_width,bin_height)
     original_img_pixels,pixel_offset = add_data_size (original_img_pixels,pixel_offset,bin_size)
-    # size_payload = bin(num_payload_pixels)
-    pass
+    
+    return original_img_pixels,pixel_offset
+
+def pixel_MSB(pixel):
+    r,g,b = pixel
+    
+    r = bin(r)[2:].rjust(8,'0')
+    g = bin(g)[2:].rjust(8,'0')
+    b = bin(b)[2:].rjust(8,'0')
+    
+    r1 = int(r[0:2],2)
+    r2 = int(r[2:4],2)
+    
+    g1 = int(g[0:2],2)
+    g2 = int(g[2:4],2)
+    
+    b1 = int(b[0:2],2)
+    b2 = int(b[2:4],2)
+    
+    return ((r1,g1,b1),(r2,g2,b2))
+
+def encode_data(original_pixel,half_img_pixel):
+    r,g,b = original_pixel
+    r1,g1,b1 = half_img_pixel
+    
+    r = r>>2
+    r = r<<2
+    r += r1
+    
+    g = g>>2
+    g = g<<2
+    g += g1
+    
+    b = b>>2
+    b = b<<2
+    b += b1
+    
+    return (r,g,b)
